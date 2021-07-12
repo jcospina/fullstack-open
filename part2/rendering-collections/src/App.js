c37b6e1faca41cb2499ab13cc161aa5d1b4cc1b5
Juan Camilo Ospina <juan.ospina.quintero@gmail.com>
2021-07-12 18:23:36 -0500

    Part 2B

--- part2/rendering-collections/src/App.js
+++ part2/rendering-collections/src/App.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('A new note');
+  const [showAll, setShowAll] = useState(true);
+  const addNote = (event) => {
+    // submit event reloads the page so preventDefault is to prevent such behaviour
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+    // use concat instead of push since it returns a new array
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  };
+  const onNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important);
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={onNoteChange}/>
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 };
