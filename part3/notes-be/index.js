17f94c6771a4e0beeb8669b1255446a80bb6cdcb
Juan Ospina <juan.ospina.quintero@gmail.com>
2021-08-10 17:22:11 -0500

    Notes with mongo

--- part3/notes-be/index.js
+++ part3/notes-be/index.js
@@ -1,31 +1,11 @@
-const { response } = require("express");
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const Note = require("./models/note");
+
 // Creates express app
 const app = express();
-
-const PORT = 3001;
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -35,13 +15,13 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (req, res) => {
-  const id = +req.params.id;
-  const note = notes.find((note) => note.id === id);
-  return note ? res.json(note) : res.status(404).end();
+  Note.findById(req.params.id).then((note) => res.json(note));
 });
 
 app.delete("/api/notes/:id", (req, res) => {
@@ -57,14 +37,12 @@ app.post("/api/notes", (req, res) => {
       error: "content missing",
     });
   }
-  const note = {
-    id: generateId(),
+  const note = new Note({
     content: body.content,
     date: new Date(),
     important: body.important || false,
-  };
-  notes.push(note);
-  res.json(note);
+  });
+  note.save().then((savedNote) => res.json(savedNote));
 });
 
 app.listen(PORT, () => {
